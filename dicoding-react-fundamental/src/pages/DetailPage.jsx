d989eca300ad98f11bb70f1988fc717da185b499
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2024-06-01 18:24:45 +0700

    fix for attempt 3

--- dicoding-react-fundamental/src/pages/DetailPage.jsx
+++ dicoding-react-fundamental/src/pages/DetailPage.jsx
@@ -1,17 +1,39 @@
 import { useParams } from "react-router-dom";
-import { getNote } from "../utils/local-data";
 import { showFormattedDate } from "../utils";
 import { Header } from "../components/Header";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { ThemeContext } from "../context/ThemeCotext";
 
 export const DetailPage = () => {
-    const { id } = useParams()
-    const note = getNote(id)
+    const [note, setNote] = useState({});
+    const { id } = useParams();
+    const accessToken = localStorage.getItem('accessToken');
+    const { theme } = useContext(ThemeContext);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://notes-api.dicoding.dev/v1/notes/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                });
+                if (response.status === 200){
+                    setNote(response.data.data);
+                }
+            } catch (error) {
+                window.alert(error);
+            }
+        };
+        fetchData();
+    }, [id, accessToken]);
 
     return (
     <>
         <Header/>
-        <div className="dark:bg-gray-800">
-            <div className="mx-40 pt-20 dark:text-white text-slate-600 ">            
+        <div className={`min-h-screen ${theme === 'dark' ? 'dark:bg-gray-800 dark:text-white' : 'bg-white text-black'}`}>
+            <div className="mx-40 pt-20">
                 <h1 className="text-4xl">{note.title}</h1>
                 <h3>{showFormattedDate(note.createdAt)}</h3>
                 <p className="text-2xl">{note.body}</p>
@@ -19,4 +41,4 @@ export const DetailPage = () => {
         </div>
     </>
     );
-}
+};
